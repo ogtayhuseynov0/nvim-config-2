e79bad4b825f68b9b6e51b4ca9c3f576b85f1b81
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2024-05-09 14:01:56 +0400

    add lazy git

--- init.lua
+++ init.lua
@@ -338,6 +338,27 @@ require('lazy').setup({
   {
     'prisma/vim-prisma',
   },
+  {
+    {
+      'kdheepak/lazygit.nvim',
+      cmd = {
+        'LazyGit',
+        'LazyGitConfig',
+        'LazyGitCurrentFile',
+        'LazyGitFilter',
+        'LazyGitFilterCurrentFile',
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+      },
+      -- setting the keybinding for LazyGit with 'keys' is recommended in
+      -- order to load the plugin when the command is run for the first time
+      keys = {
+        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      },
+    },
+  },
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
